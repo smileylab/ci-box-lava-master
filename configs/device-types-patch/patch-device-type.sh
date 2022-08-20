cb65e2e2627f054f172abbffce063075d64b5b74
po.cheng <po.cheng@adlinktech.com>
2022-08-20 09:18:54 +0800

    device-types-patch: script to update device types
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

---
+++ configs/device-types-patch/patch-device-type.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DEVTYPE_PATH=/etc/lava-server/dispatcher-config/device-types/
+if [ -e /usr/share/lava-server/device-types/ ];then
+	DEVTYPE_PATH=/usr/share/lava-server/device-types/
+fi
+
+cd $DEVTYPE_PATH
+for patch in $(ls /root/device-types-patch/*patch)
+do
+	echo "DEBUG: patch with $patch"
+	sed -i 's,lava_scheduler_app/tests/device-types/,,' $patch
+	sed -i 's,etc/dispatcher-config/device-types/,,' $patch
+	patch -p1 < $patch || exit $?
+done
+chown -R lavaserver:lavaserver $DEVTYPE_PATH
